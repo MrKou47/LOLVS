fccb34260b34d4890537c0c064d13961541274f5
Mrkou47 <koubodehao@gmail.com>
2018-02-22 18:57:10 +0800

    fix hero file pwd

--- src/server/api/heros/index.ts
+++ src/server/api/heros/index.ts
@@ -1,1 +1,21 @@
+import { Router } from 'express';
+
 import { heros } from '../../db';
+import genMsg from '../utils/gen-msg';
+
+const herosRouter = Router();
+
+// TODO: search hero
+herosRouter.get('/search', (req, res, next) => {
+  res.send('');
+});
+
+// TODO: add hero
+herosRouter.post('/add', (req, res, next) => {
+  res.send('');
+});
+
+// TODO: add hero's shortcut
+herosRouter.post('/shortcut/add', (req, res, next) => {
+  res.send('');
+});
